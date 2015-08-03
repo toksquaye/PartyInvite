c4e9d1d52920ba53bdd0836227b8d7e20b121316
Tokunbo Quaye <toksquaye@yahoo.com>
2015-08-03 02:26:26 -0400

    Created controllers and view for simple party invite site.

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using PartyInvites.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                //todo : email response to party organizer
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                //there isa validation error, so return the form
+                return View();
+            }
+        }
+    }
+}
