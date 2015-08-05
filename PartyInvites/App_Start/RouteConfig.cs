ea561cb789dc8760c6e800cd232e653d9b292b7c
Tokunbo Quaye <toksquaye@yahoo.com>
2015-08-05 15:58:41 -0400

    Add login functionality to check guest name against database. Start view layout for main site area

--- PartyInvites/App_Start/RouteConfig.cs
+++ PartyInvites/App_Start/RouteConfig.cs
@@ -13,11 +13,15 @@ namespace PartyInvites
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
+            /*routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            );*/
+
+            routes.MapRoute("Home", "", new { controller = "Home", action = "Index" });
+            routes.MapRoute("RSVP", "rsvp", new { controller = "Home", action = "RsvpForm"});
+            routes.MapRoute("Login", "login", new { controller = "Auth", action = "Login" });
         }
     }
 }
---
+++ PartyInvites/Controllers/AuthController.cs
@@ -0,0 +1,55 @@
+﻿using PartyInvites.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using NHibernate.Linq;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class AuthController : Controller
+    {
+        // GET: Auth
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(AuthLogin form)
+        {
+            var guest = Database.Session.Query<Guest>().FirstOrDefault(g => g.Name == form.Name);
+
+            //this is to prevent timing attacks during login
+            //the time to check for a user that isn't in the db and the time
+            //that checks for a user that is in the db is now similar
+            //if (user == null)
+                //SimpleBlog.Models.User.FakeHash();
+
+            if (guest == null)
+                ModelState.AddModelError("Name", "Invalid Name Entered");
+
+            if (!ModelState.IsValid) //if validation check fails
+                return View(form);  //give user form back
+
+
+
+            //tell asp.net a person is who he says he is. letting asp know what user is logged in
+            FormsAuthentication.SetAuthCookie(guest.Name, true);
+
+            //if (!string.IsNullOrWhiteSpace(returnUrl))
+            //    return Redirect(returnUrl);
+
+            return RedirectToRoute("rsvp");
+            
+        }
+    }
+}
